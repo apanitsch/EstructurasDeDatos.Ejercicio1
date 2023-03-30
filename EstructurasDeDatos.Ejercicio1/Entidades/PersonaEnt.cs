4b2fc08d8d4a0f449d04eabb6f09a5354e39fe99
apanitsch <apanitsch@gmail.com>
2023-03-30 20:52:50 -0300

    Ejemplo incompleto ingreso personas.

---
+++ EstructurasDeDatos.Ejercicio1/Entidades/PersonaEnt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructurasDeDatos.Ejercicio1.Entidades;
+
+internal class PersonaEnt
+{
+    public long NumeroDocumento { get; set; }
+    public string Nombre { get; set;}
+    public string Apellido { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+}
